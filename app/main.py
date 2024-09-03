e37ee67b8332bd3e008647e4fa515ba59d04b4a9
alamin40 <hossain.78295@gmail.com>
2024-09-03 05:21:32 +0200

    adding code into main file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
+from re import template
 from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+
+
+# Setup for Jinja2 templates
+templates = Jinja2Templates(directory="templates")
 
 
 app = FastAPI()
+
+@app.get('/index', response_class=HTMLResponse)
+def index(request: Request):
+  return templates. TemplateResponse("index.html", {"request": request})
