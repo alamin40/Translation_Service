9dc8f90fb14fe62bd62880848b90b9bc1b8f46a2
alamin40 <hossain.78295@gmail.com>
2024-09-03 05:15:24 +0200

    adding code into main file

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
