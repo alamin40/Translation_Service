cf2c336d267a1c1d56c42abcb910b7ac9ab5aa59
alamin40 <hossain.78295@gmail.com>
2024-09-05 05:14:18 +0200

    adding code in main.py

--- app/main.py
+++ app/main.py
@@ -37,3 +37,67 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/index", response_class=HTMLResponse)
 async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+
+
+
+
+
+# @app.post("/translate")
+# async def translate(request: Request, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+#     try:
+#         body = await request.json()
+#         logging.info(f"Received raw request body: {body}")
+#     except Exception as e:
+#         logging.error(f"Error parsing request body: {e}")
+#         raise HTTPException(status_code=400, detail="Invalid request body")
+    
+#     try:
+#         translation_request = TranslationRequestSchema(**body)
+#         logging.info(f"Validated request data: {translation_request.json()}")
+#     except Exception as e:
+#         logging.error(f"Error validating request data: {e}")
+#         raise HTTPException(status_code=422, detail="Unprocessable Entity")
+    
+#     # Convert the languages string to a list
+#     languages_list = [lang.strip() for lang in translation_request.languages.split(',')]
+    
+#     request_data = TranslationRequest(
+#         text=translation_request.text,
+#         languages=languages_list
+#     )
+#     db.add(request_data)
+#     db.commit()
+#     db.refresh(request_data)
+#     background_tasks.add_task(process_translations, request_data.id, translation_request.text, languages_list)
+#     return {"id": request_data.id, "status": request_data.status}
+
+
+@app.post("/translate")
+async def translate(request: TranslationRequestSchema, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+    print(request.text)
+    print(request.languages)
+
+    # requests above is a pydantic model, my_dict below is a dict, incase you need to use one or another
+    my_dict = request.model_dump()
+#     print(my_dict)
+    ########################################################
+    ########################################################
+
+    request_data = models.TranslationRequest(
+        text=request.text,
+        languages=request.languages)
+    db.add(request_data)
+    db.commit()
+    db.refresh(request_data)
+
+    #background_tasks.add_task(process_translations, request_data.id, request.text, request.languages)
+    return {"payload": request_data}
+
+
+
