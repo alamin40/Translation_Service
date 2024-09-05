6c2df320bc9f638c1427bf7deb6f5f4d210be02e
alamin40 <hossain.78295@gmail.com>
2024-09-05 05:12:58 +0200

    adding code in main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
+import logging
+from fastapi import FastAPI, BackgroundTasks, HTTPException, Depends, Request, status
 from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+
+from schemas import TranslationRequestSchema
+from typing import List
+from utils import translate_text, process_translations
+
+# db related #  #
+from database import engine, SessionLocal, get_db
+import models 
+from models import TranslationRequest, TranslationResult, IndividualTranslations
+models.Base.metadata.create_all(engine)
+#       #       #
 
 # Setup for Jinja2 templates
 templates = Jinja2Templates(directory="templates")
